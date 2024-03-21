2d364e554543cc0422d742fb13faf44f922fd3b1
krishnnasarrdah <sarrdah.k@northeastern.edu>
2024-03-20 23:28:42 -0400

    db connection done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
+import { db } from "../db";
 
-export default function Home() {
+export default async function Home() {
+
+  const items = await db.query.testing.findMany({});
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      {
+        items.map((item) => (
+          <div key={item.id} className="flex flex-col items-center justify-center">
+            <h1 className="text-2xl font-bold">{item.name}</h1>
+          </div>
+        ))
+      }
     </main>
   );
 }
