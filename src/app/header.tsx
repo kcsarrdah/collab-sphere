20c98ee469e26d751bbde9b75cf65c6670c73769
krishnnasarrdah <sarrdah.k@northeastern.edu>
2024-03-31 18:17:46 -0400

    added a landing page and also need to fix the display logo

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -45,9 +45,9 @@ function AccountDropdown() {
 
 export function Header() {
   const session = useSession();
-
+  const isLoggedIn = !!session.data;
   return (
-    <header className="bg-gray-100 py-2 dark:bg-gray-900 container mx-auto">
+    <header className="bg-gray-100 py-2 dark:bg-gray-900 container mx-auto z-10 relative">
       <div className="flex justify-between items-center">
         <Link
           href="/"
@@ -58,20 +58,26 @@ export function Header() {
         </Link>
 
         <nav>
-          <Link className="mr-4 hover:underline"
-          href="/your-rooms">Your Rooms</Link>
+          {isLoggedIn && (
+            <>
+              <Link className="mr-4 hover:underline" href="/browse">
+                Browse
+              </Link>
+              <Link className="mr-4 hover:underline" href="/your-rooms">
+                Your Rooms
+              </Link>
+            </>
+          )}
         </nav>
 
         <div className="flex items-center gap-4">
-          {session.data && <AccountDropdown />}
-          {!session.data && (
-          <Button onClick={() => signIn()}>
-            <LogInIcon className="mr-2" />
-            Sign In
-          </Button>
-          )
-
-          }
+          {isLoggedIn && <AccountDropdown />}
+          {!isLoggedIn && (
+            <Button onClick={() => signIn()}>
+              <LogInIcon className="mr-2" />
+              Sign In
+            </Button>
+          )}
           <ModeToggle />
         </div>
       </div>
